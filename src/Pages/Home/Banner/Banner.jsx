1bb329eec58a343de257f4f0583f7051c14de2c9
Hedayetul-Islam <hedayetul36@gmail.com>
2023-06-15 18:53:28 +0600

    banner design completed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,22 +5,16 @@ const Banner = () => {
             <div className="carousel w-full">
                 <div id="item1" className="carousel-item w-full">
                     <img src="https://images.pexels.com/photos/97353/pexels-photo-97353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
+                    <div className="absolute rounded-xl flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 pl-12 w-1/2'>
+                        <h2 className='text-6xl font-bold'>Attractive Baby Toy Cars</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quas voluptate reiciendis eligendi exercitationem veniam.</p>
+                        <div>
+                            <button className="btn btn-primary">Discover More</button>
+                        </div>
+                    </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
-                    <img src="https://images.pexels.com/photos/574519/pexels-photo-574519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
                 </div>
-                <div id="item3" className="carousel-item w-full">
-                    <img src="https://images.pexels.com/photos/35964/ferrari-red-auto-sports-car.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
-                </div>
-                <div id="item4" className="carousel-item w-full">
-                    <img src="https://images.pexels.com/photos/4934421/pexels-photo-4934421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
-                </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-                <a href="#item4" className="btn btn-xs">4</a>
             </div>
         </div>
     );
