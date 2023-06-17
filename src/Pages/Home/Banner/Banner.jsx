1d5f4df8961eb53283a5458c26509d9d626c2f7c
Hedayetul-Islam <hedayetul36@gmail.com>
2023-06-17 23:10:20 +0600

    Gallery section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
                 <div id="item1" className="carousel-item w-full">
                     <img src="https://images.pexels.com/photos/97353/pexels-photo-97353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
                     <div className="absolute rounded-xl flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                    <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Attractive Baby Toy Cars</h2>
+                    <div className='text-white space-y-7 pl-12 w-2/3'>
+                        <h2 className='text-7xl font-bold'>Attractive Baby Toy Cars</h2>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quas voluptate reiciendis eligendi exercitationem veniam.</p>
                         <div>
                             <button className="btn btn-primary">Discover More</button>
---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,35 @@
+
+const Gallery = () => {
+    return (
+        <div className="bg-slate-200">
+            <div className="mt-12 text-center p-6">
+                <h2 className="uppercase text-6xl font-semibold">Gallery of toy cars</h2>
+                <p className="p-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis atque quasi doloremque? In laboriosam aspernatur esse, obcaecati alias suscipit quod culpa ipsam est nulla explicabo?</p>
+            </div>
+            <div>
+                <div className="carousel w-full">
+                    <div id="item1" className="carousel-item w-full">
+                        <img src="https://images.pexels.com/photos/2527931/pexels-photo-2527931.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
+                    </div>
+                    <div id="item2" className="carousel-item w-full">
+                        <img src="https://images.pexels.com/photos/102977/pexels-photo-102977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
+                    </div>
+                    <div id="item3" className="carousel-item w-full">
+                        <img src="https://images.pexels.com/photos/1648349/pexels-photo-1648349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
+                    </div>
+                    <div id="item4" className="carousel-item w-full">
+                        <img src="https://images.pexels.com/photos/249343/pexels-photo-249343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full" />
+                    </div>
+                </div>
+                <div className="flex justify-center w-full py-2 gap-2">
+                    <a href="#item1" className="btn btn-xs">1</a>
+                    <a href="#item2" className="btn btn-xs">2</a>
+                    <a href="#item3" className="btn btn-xs">3</a>
+                    <a href="#item4" className="btn btn-xs">4</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
